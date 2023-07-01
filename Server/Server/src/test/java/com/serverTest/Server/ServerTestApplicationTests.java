996ff4fbb63bb497d41e38f91e059814c431a1da
Tanmay Dubey <tanmaydubey1947@gmail.com>
2023-07-01 23:15:00 +0530

    initial commit

---
+++ Server/Server/src/test/java/com/serverTest/Server/ServerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serverTest.Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
